f142f6013091b8f46ef5d747964affce9738d274
Shimul <shimulmohammad420@gmail.com>
2023-10-20 19:50:52 +0600

    no cart product text display updated when there is no product in cart

--- src/Pages/MyCart.jsx
+++ src/Pages/MyCart.jsx
@@ -43,7 +43,7 @@ const MyCart = () => {
                                 'Your Coffee has been deleted.',
                                 'success'
                             )
-                            const remaining = products?.filter(product => product._id!== _id);
+                            const remaining = products?.filter(product => product._id !== _id);
                             setCartProducts(remaining);
                         }
                     })
@@ -54,22 +54,28 @@ const MyCart = () => {
     return (
         <div className="container mx-auto">
             <div className="overflow-x-auto py-10 mt-10 mb-32  rounded-lg shadow-xl">
-                <table className="table text-center">
+
+                < table className="table text-center" >
                     <thead>
-                        <tr>
-                            <th  className="text-xl">Product Name</th>
-                            <th className="text-xl hidden md:block">Brand & Type</th>
-                            <th className="text-xl">Price</th>
-                            <th className="text-xl">Delete</th>
-                        </tr>
+                        {products.length > 0 && (
+                            <tr>
+                                <th className="text-xl">Product Name</th>
+                                <th className="text-xl hidden md:block">Brand & Type</th>
+                                <th className="text-xl">Price</th>
+                                <th className="text-xl">Delete</th>
+                            </tr>
+                        )}
                     </thead>
                     <tbody>
-                        {
-                            products.length === 0 ? <>
-                                <p className="text-center font-medium text-2xl md:text-4xl">You have not added any product to the cart</p>
-                            </>
-                                :
-                                products?.map(product => <tr className="hover text-center" key={product._id}>
+                        {products.length === 0 ? (
+                            <tr>
+                                <td colSpan="4" className="text-center font-medium text-2xl md:text-4xl">
+                                    You have not added any product to the cart
+                                </td>
+                            </tr>
+                        ) : (
+                            products?.map((product) => (
+                                <tr className="hover text-center" key={product._id}>
                                     <td className="flex justify-center">
                                         <div className="flex items-center space-x-3">
                                             <div className="avatar">
@@ -89,16 +95,20 @@ const MyCart = () => {
                                     </td>
                                     <td className="md:text-xl text-base font-semibold">$ {product?.price}</td>
                                     <th>
-                                        <button onClick={() => handleDelete(product?._id)} className="btn text-xl text-red-600"><RiDeleteBin6Line></RiDeleteBin6Line></button>
+                                        <button
+                                            onClick={() => handleDelete(product?._id)}
+                                            className="btn text-xl text-red-600"
+                                        >
+                                            <RiDeleteBin6Line></RiDeleteBin6Line>
+                                        </button>
                                     </th>
-
-                                </tr>)
-
-                        }
-
+                                </tr>
+                            ))
+                        )}
                     </tbody>
+                </table >
 
-                </table>
+
             </div>
         </div>
     );
